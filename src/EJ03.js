3ff8844dc516af6d5ad0e6249342b30462ec0118
LUCAS IOEL ROITMAN <49518218@ort>
2026-03-30 08:41:02 -0300

    EJ03#

---
+++ src/EJ03.js
@@ -0,0 +1,6 @@
+import Alumno from "./models/alumno.js";
+
+let lucas = new Alumno("luquiGamer2009", 10000);
+let andres = new Alumno("andyGamer2009", 10001);
+
+console.log("WOWWWWW no puedo decidirme entre cual es mejor, si " + lucas.username + " o " + andres.username);
